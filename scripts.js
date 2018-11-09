5efa30d0603a5b1d125b1baa7305ac9c0f58da60
Kim Duclos <kimduclos@gmail.com>
2018-11-08 20:34:18 -0700

    infoBox fade-in working

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function(){  // fade-out animation of titleCard class
     $(window).scroll(function(){
         $(".titleCard").css("opacity", 1 - $(window).scrollTop() / ($('.titleCard').height() / 2));
     });
 });
+
+$(window).on("load",function() {
+    $(window).scroll(function() {
+      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      $(".infoBox").each(function() {
+        /* Check the location of each desired element */
+        var objectBottom = $(this).offset().top + $(this).outerHeight();
+        
+        /* If the element is completely within the window, fade it in */
+        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+          if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+        } else { //object goes out of view (scrolling up)
+          if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        }
+      });
+    }).scroll(); //invoke scroll-handler on page-load
+  });
